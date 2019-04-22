665a16749f9c2cc5df66a55804186411f429990d
Michael Townsend <michael@doublespeakgames.com>
2019-04-22 11:41:44 -0400

    Disable GPIO mock

--- src/constants.mjs
+++ src/constants.mjs
@@ -4,7 +4,7 @@
  * @since April 2019
  */
 
-export const MOCK_GPIO = true;
+export const MOCK_GPIO = false;
 export const OUTPUT_PIN = 4;
 export const TRIGGER_PINS = [ 5, 6, 13, 19 ];
 export const COMPARTMENT_PIN = 26;
