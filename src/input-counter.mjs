96e004653527b15b947ae57f0a7d4a39000b0cd5
Michael Townsend <michael@doublespeakgames.com>
2019-04-22 23:42:46 -0400

    I guess onoff doesn't bind its event listeners. Oh well. Fixes #2.

--- src/input-counter.mjs
+++ src/input-counter.mjs
@@ -11,13 +11,12 @@ import { TRIGGER_PINS } from './constants';
 const INPUTS = TRIGGER_PINS.map(pin => connect(pin, 'in', 'rising'));
 
 export default function InputCounter(changeHandler) {
-
   let count = 0;
-  function increment() {
-    changeHandler(++count);
-    this.unwatch(increment);
-  };
-
-  INPUTS.forEach(p => p.watch(increment));
+  INPUTS.forEach(p => {
+    p.watch(function increment() {
+      changeHandler(++count);
+      p.unwatch(increment);
+    });
+  });
 }
 
