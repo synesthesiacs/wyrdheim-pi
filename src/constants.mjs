162cd9a64eadb8bd8fc3b0a0e1d29a6a8c939531
Michael Townsend <michael@doublespeakgames.com>
2019-04-23 22:01:56 -0400

    Brute force memory leak prevention, lol.

--- src/constants.mjs
+++ src/constants.mjs
@@ -10,3 +10,4 @@ export const TRIGGER_PINS = [ 18, 12, 13, 19 ];
 export const COMPARTMENT_PIN = 26;
 export const NUM_DRONES = 5;
 export const DEBOUNCE_WINDOW = 500; // ms
+export const MAX_PLAYER_LIFE = 3 * 60 * 60 * 1000; // ms
