e011a4fa9695ae04c0d402ef2534af5358ffb272
Michael Townsend <michael@doublespeakgames.com>
2019-04-24 21:45:23 -0400

    Ensure trigger sound plays on every GPIO signal. Closes #6.

--- src/main.mjs
+++ src/main.mjs
@@ -6,31 +6,34 @@
 
 import InputCounter from './input-counter';
 import { connect } from './gpio';
-import { OUTPUT_PIN, COMPARTMENT_PIN, NUM_DRONES, DEBOUNCE_WINDOW } from './constants';
+import { TRIGGER_PINS, OUTPUT_PIN, COMPARTMENT_PIN, NUM_DRONES, DEBOUNCE_WINDOW } from './constants';
 import { setDrone, onRuneTriggered, onCompartmentTriggered, stop } from './audio-controller';
 import { debounced } from './util';
 
 const OUTPUT = connect(OUTPUT_PIN, 'out');
 const COMPARTMENT = connect(COMPARTMENT_PIN, 'in', 'rising');
+const TRIGGERS = TRIGGER_PINS.map(pin => connect(pin, 'in', 'rising'));
 
 function cleanup() {
   console.log('Daisy, Daisy, give me your answer do...')
   stop();
 }
 
-// Set up the incrementing drone loops
+// Rune trigger sounds
+TRIGGERS.forEach(t => t.watch(debounced(onRuneTriggered, DEBOUNCE_WINDOW)));
+
+// Set up the compartment trigger sound
+COMPARTMENT.watch(debounced(onCompartmentTriggered, DEBOUNCE_WINDOW));
+
+// Set up the drone incrementer
 setDrone(0);
-new InputCounter(async num => {
-  onRuneTriggered();
+new InputCounter(TRIGGERS, async num => {
   setDrone(num);
   if (num >= NUM_DRONES - 1) {
     // Fully incremented, so signal the servo arduino
     OUTPUT.write(1);
   }
 });
-
-// Set up the compartment trigger sound
-COMPARTMENT.watch(debounced(onCompartmentTriggered, DEBOUNCE_WINDOW));
 
 // Make sure everything stops when the app is closed
 process
