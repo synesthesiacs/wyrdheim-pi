16767873e7998e8b73db3615f2aa9b0fc1c4e129
Michael Townsend <michael@doublespeakgames.com>
2019-04-30 14:03:29 -0400

    Added pulse decoder for drone selection

--- src/main.mjs
+++ src/main.mjs
@@ -3,16 +3,18 @@
  * @author mtownsend
  * @since April 2019
  */
-
+ 
+import { TRIGGER_PINS, OUTPUT_PIN, COMPARTMENT_PIN, SELECTOR_PIN, NUM_DRONES, DEBOUNCE_WINDOW } from './constants';
 import InputCounter from './input-counter';
+import PulseDecoder from './pulse-decoder';
 import { connect } from './gpio';
-import { TRIGGER_PINS, OUTPUT_PIN, COMPARTMENT_PIN, NUM_DRONES, DEBOUNCE_WINDOW } from './constants';
 import { setDrone, onRuneTriggered, onCompartmentTriggered, stop } from './audio-controller';
 import { debounced } from './util';
 
-const OUTPUT = connect(OUTPUT_PIN, 'out');
-const COMPARTMENT = connect(COMPARTMENT_PIN, 'in', 'rising');
-const TRIGGERS = TRIGGER_PINS.map(pin => connect(pin, 'in', 'rising'));
+const output = connect(OUTPUT_PIN, 'out');
+const compartment = connect(COMPARTMENT_PIN, 'in', 'rising');
+const triggers = TRIGGER_PINS.map(pin => connect(pin, 'in', 'rising'));
+const selector = connect(SELECTOR_PIN, 'in', 'rising');
 
 function cleanup() {
   console.log('Daisy, Daisy, give me your answer do...')
@@ -20,19 +22,24 @@ function cleanup() {
 }
 
 // Rune trigger sounds
-TRIGGERS.forEach(t => t.watch(debounced(onRuneTriggered, DEBOUNCE_WINDOW)));
+triggers.forEach(t => t.watch(debounced(onRuneTriggered, DEBOUNCE_WINDOW)));
 
 // Set up the compartment trigger sound
-COMPARTMENT.watch(debounced(onCompartmentTriggered, DEBOUNCE_WINDOW));
+compartment.watch(debounced(onCompartmentTriggered, DEBOUNCE_WINDOW));
 
 // Set up the drone incrementer
 setDrone(0);
-new InputCounter(TRIGGERS, async num => {
+new InputCounter(triggers, num => {
   setDrone(num);
   if (num >= NUM_DRONES - 1) {
     // Fully incremented, so signal the servo arduino
     OUTPUT.write(1);
   }
+});
+
+// Set up the post-rune drone selector
+new PulseDecoder(selector, index => {
+  console.log(`Select drone ${index}.`); // TODO
 });
 
 // Make sure everything stops when the app is closed
