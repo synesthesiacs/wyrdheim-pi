5795be76bb31023c4379fa63c005b59f169c33ac
Michael Townsend <michael@doublespeakgames.com>
2019-05-07 17:45:32 -0400

    cleanup

--- src/constants.mjs
+++ src/constants.mjs
@@ -6,7 +6,6 @@
 
 export const MOCK_GPIO = false;
 export const OUTPUT_PIN = 4;
-//export const TRIGGER_PINS = [ 18, 12, 13, 19 ];
 export const TRIGGER_PINS = [ 5, 6, 13, 19 ];
 export const COMPARTMENT_PIN = 26;
 export const SELECTOR_PIN = 16;
