37bf151fc41129cf9ae54d9ed02c1625317e5dc7
Michael Townsend <michael@doublespeakgames.com>
2019-05-09 11:36:15 -0400

    Play nicer with real Arduinos

--- src/main.mjs
+++ src/main.mjs
@@ -4,7 +4,7 @@
  * @since April 2019
  */
  
-import { TRIGGER_PINS, OUTPUT_PIN, COMPARTMENT_PIN, SELECTOR_PIN, DEBOUNCE_WINDOW } from './constants';
+import { BOOT_DELAY, TRIGGER_PINS, OUTPUT_PIN, COMPARTMENT_PIN, SELECTOR_PIN, DEBOUNCE_WINDOW } from './constants';
 import InputCounter from './input-counter';
 import PulseDecoder from './pulse-decoder';
 import { connect } from './gpio';
@@ -17,31 +17,40 @@ const compartment = connect(COMPARTMENT_PIN, 'in', 'rising');
 const triggers = TRIGGER_PINS.map(pin => connect(pin, 'in', 'rising'));
 const selector = connect(SELECTOR_PIN, 'in', 'rising');
 
+let phase2 = false;
+
 function cleanup() {
   console.log('Daisy, Daisy, give me your answer do...')
   stop();
 }
 
-// Rune trigger sounds
-triggers.forEach(t => t.watch(debounced(onRuneTriggered, DEBOUNCE_WINDOW)));
-
-// Set up the compartment trigger sound
-compartment.watch(debounced(onCompartmentTriggered, DEBOUNCE_WINDOW));
-
-// Set up the drone incrementer
 setDrone(0);
-new InputCounter(triggers, num => {
-  setDrone(num);
-  if (num >= triggers.length) {
-    // Fully incremented, so signal the servo arduino
-    output.write(1);
-  }
-});
 
-// Set up the post-rune drone selector
-new PulseDecoder(selector, index => {
-  setDrone(triggers.length + clamp(index, 1, 4) - 1);
-});
+// Delay connecting to GPIO to ignore Arduino boot noise
+setTimeout(() => {
+
+  // Rune trigger sounds
+  triggers.forEach(t => t.watch(debounced(onRuneTriggered, DEBOUNCE_WINDOW)));
+
+  // Set up the compartment trigger sound
+  compartment.watch(debounced(onCompartmentTriggered, DEBOUNCE_WINDOW));
+
+  // Set up the drone incrementer
+  new InputCounter(triggers, num => {
+    setDrone(num);
+    if (num >= triggers.length) {
+      // Fully incremented, so signal the servo arduino
+      phase2 = true;
+      output.write(1);
+    }
+  });
+
+  // Set up the post-rune drone selector
+  new PulseDecoder(selector, index => {
+    phase2 && setDrone(triggers.length + clamp(index, 1, 4) - 1);
+  });
+
+}, BOOT_DELAY);
 
 // Make sure everything stops when the app is closed
 process
