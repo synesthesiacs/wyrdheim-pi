dc0bba1ece0c3718f62a7e7127efdf5c6fa34483
Michael Townsend <michael@doublespeakgames.com>
2019-05-09 11:36:15 -0400

    Play nicer with real Arduinos

--- src/constants.mjs
+++ src/constants.mjs
@@ -5,6 +5,7 @@
  */
 
 export const MOCK_GPIO = false;
+export const BOOT_DELAY = 10 * 1000; // ms
 export const OUTPUT_PIN = 4;
 export const TRIGGER_PINS = [ 5, 6, 13, 19 ];
 export const COMPARTMENT_PIN = 26;
