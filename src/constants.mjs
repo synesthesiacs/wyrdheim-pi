3fddaf797af00f3cef5c90bb7b1cc354cb7a63c1
acarito <acarito@carito.ca>
2019-05-25 04:53:19 +0000

    updated GPIO pins 5 and 6 to 23 and 24. not sure why the former stopped working

--- src/constants.mjs
+++ src/constants.mjs
@@ -7,7 +7,7 @@
 export const MOCK_GPIO = false;
 export const BOOT_DELAY = 10 * 1000; // ms
 export const OUTPUT_PIN = 4;
-export const TRIGGER_PINS = [ 5, 6, 13, 19 ];
+export const TRIGGER_PINS = [ 23, 24, 13, 19 ];
 export const COMPARTMENT_PIN = 26;
 export const SELECTOR_PIN = 16;
 export const TIMER = 12 * 60 * 60 * 1000;
